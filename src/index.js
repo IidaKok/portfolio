abfe764476e3f2ceed81a9696096d55c5914ee74
IidaKok <82649942+IidaKok@users.noreply.github.com>
2023-01-28 21:10:51 +0200

    Routerin lisäys indexiin

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import 'animate.css';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+<Router> 
+<Routes>
+ <Route path="/" element={<App />}> 
+<Route exact path="/portfolio" element={<Home />} />
+ <Route exact path="/portfolio/about" element={<About />} />
+<Route exact path="/portfolio/projects" element={<Projects />} />
+<Route exact path="/portfolio/contact" element={<Contact />} />
+
+ </Route> 
+</Routes> 
+</Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
